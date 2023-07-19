9cb12700689a04481997da6d521917df363ff08d
zhangchao <zhangchao338@huawei.com>
2023-07-19 23:53:35 +0800

    add dyxs

---
+++ ssr/js/dyxs.js
@@ -0,0 +1,28 @@
+const domain = 'dyxs28.com';
+
+const url = $request.url;
+const method = $request.method;
+
+if (!$response.body) {
+    $done({});
+}
+
+let body = $response.body;
+
+if (method === "GET") {
+	if (url.includes(domain + "/statics/js/main.js")) {
+		body = body.replace(/<div.*\/div>/, '');
+	}
+	if (url.includes(domain + "/paly")) {
+		body = body.replace('class="player-rm rm-two rm-list"', 'style="visibility:hidden;"');
+		body = body.replace('id="rm-float"', 'style="visibility:hidden;"');
+	}
+	if (url.includes(domain + "/show")) {
+		body = body.replace('class="module-domain-prompt"', 'style="visibility:hidden;"');
+		body = body.replace('class="player-rm rm-two rm-list"', 'style="visibility:hidden;"');
+	}
+
+}
+$done({
+    body
+});
